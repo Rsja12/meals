81612da0ed56b62128874fa8b9871d3d766a824f
Rsja12 <asocarras10@gmail.com>
2020-08-31 17:04:16 -0400

    add in functionality for splashscreen to remain rendered until fonts have loaded

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+
+const fetchFonts = () => {
+    Font.loadAsync({
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    });
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [isFontLoaded, setIsFontLoaded] = useState(false);
+
+    if (!isFontLoaded) {
+        return (
+            <AppLoading
+                startAsync={fetchFonts}
+                onFinish={() => setIsFontLoaded(true)}
+            />
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Welcome to Meals!</Text>
+            <StatusBar style='auto' />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
