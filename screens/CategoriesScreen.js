44084d37f5c41664cf3e087d7ca034fb30c91a88
Rsja12 <asocarras10@gmail.com>
2020-09-21 19:37:43 -0400

    update navbar title to head back to all categories

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -37,7 +37,7 @@ const CategoriesScreen = (props) => {
 };
 
 CategoriesScreen.navigationOptions = {
-    headerTitle: 'Meal Categories'
+    headerTitle: 'Categories'
 }
 
 export default CategoriesScreen;
