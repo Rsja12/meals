7c76fda4aa17a9fb0298cc14fa708429fb7e1049
Rsja12 <asocarras10@gmail.com>
2020-09-28 15:23:15 -0400

    create MealItem component template

---
+++ components/MealItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const MealItem = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default MealItem
+
+const styles = StyleSheet.create({})
