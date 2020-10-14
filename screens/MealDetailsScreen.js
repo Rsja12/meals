f33e8b1d072a5483ec72d77b918c7d88f136f3dd
Rsja12 <asocarras10@gmail.com>
2020-10-14 11:41:35 -0400

    create constant selectedMeal to find corresponding meal object from MEALS

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -5,6 +5,7 @@ import { MEALS } from '../data/dummy-data'
 
 const MealDetailsScreen = (props) => {
     const mealId = props.navigation.getParam('mealId')
+    const selectedMeal = MEALS.find(meal => meal.id === mealId)
 
     return (
         <View style={styles.screen}>
