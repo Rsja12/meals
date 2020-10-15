6392b9f46a162fabf5d038ef0aa865b9c76230c2
Rsja12 <asocarras10@gmail.com>
2020-10-15 10:08:44 -0400

    forward props to HeaderButton

--- components/CustomHeaderButton.js
+++ components/CustomHeaderButton.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import { HeaderButton } from 'react-navigation-header-buttons'
-import { Ionicons } from '@expo/vector-icons'
+import React from 'react';
+import { HeaderButton } from 'react-navigation-header-buttons';
+import { Ionicons } from '@expo/vector-icons';
 
-import Colors from '../constants/Colors'
+import Colors from '../constants/Colors';
 
 const CustomHeaderButton = (props) => {
-    return <HeaderButton />
-}
+    return <HeaderButton {...props} />;
+};
 
-export default CustomHeaderButton
-
+export default CustomHeaderButton;
