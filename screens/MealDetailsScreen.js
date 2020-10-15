c71ca32b198871267e868b6068fb742ac6506f6c
Rsja12 <asocarras10@gmail.com>
2020-10-15 10:12:19 -0400

    import HeaderButtons component in MealDetailsScreen

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { HeaderButtons } from 'react-navigation-header-buttons'
 
 import { MEALS } from '../data/dummy-data';
 
