df2bd4de59a2992bdfa5ac8ff5dc71f7cdecbffc
Rsja12 <asocarras10@gmail.com>
2020-10-15 10:11:04 -0400

    add styling props to HeaderButton

--- components/CustomHeaderButton.js
+++ components/CustomHeaderButton.js
@@ -5,7 +5,14 @@ import { Ionicons } from '@expo/vector-icons';
 import Colors from '../constants/Colors';
 
 const CustomHeaderButton = (props) => {
-    return <HeaderButton {...props} />;
+    return (
+        <HeaderButton
+            {...props}
+            IconComponent={Ionicons}
+            iconSize={23}
+            color={Colors.primary}
+        />
+    );
 };
 
 export default CustomHeaderButton;
