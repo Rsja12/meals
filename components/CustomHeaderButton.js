8ed1474e150f9b222c4d39601a731a63fecda052
Rsja12 <asocarras10@gmail.com>
2020-10-18 18:55:23 -0400

    remove color prop from CustomHeaderButton

--- components/CustomHeaderButton.js
+++ components/CustomHeaderButton.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { HeaderButton } from 'react-navigation-header-buttons';
 import { Ionicons } from '@expo/vector-icons';
 
-import Colors from '../constants/Colors'
 
 const CustomHeaderButton = (props) => {
     return (
@@ -10,7 +9,6 @@ const CustomHeaderButton = (props) => {
             {...props}
             IconComponent={Ionicons}
             iconSize={23}
-            color={Colors.primary}
         />
     );
 };
