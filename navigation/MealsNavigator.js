8b28879dcaedb4c74c744f8a32ced6b971b0534e
Rsja12 <asocarras10@gmail.com>
2020-10-20 11:14:24 -0400

    update createBottomTabNavigator import to have it work with React Navigation v4

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,6 +1,6 @@
 import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
