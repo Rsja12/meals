b1338e5a859e31ca6dc3ff52414165d0a52a691c
Rsja12 <asocarras10@gmail.com>
2020-10-20 11:03:19 -0400

    import and implement createBottomTabNavigator in MealsNavigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,15 +1,21 @@
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createAppContainer } from 'react-navigation';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailsScreen from '../screens/MealDetailsScreen';
-import { DarkTheme } from '@react-navigation/native';
+import FavoritesScreen from '../screens/FavoritesScreen'
 
 const MealsNavigator = createStackNavigator({
     Categories: CategoriesScreen,
     Meals: CategoryMealsScreen,
     MealDetails: MealDetailsScreen,
+});
+
+const MealsTabNavigator = createBottomTabNavigator({
+    Meals: MealsNavigator,
+    Favorites: FavoritesScreen
 });
 
 export default createAppContainer(MealsNavigator);
