6c2483c009cfde485a51a9e031e13276b097b7e2
Rsja12 <asocarras10@gmail.com>
2020-10-21 09:46:27 -0400

    render Flatlist and pass appropriate props in MealList

--- components/MealList.js
+++ components/MealList.js
@@ -1,14 +1,21 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View, FlatList } from 'react-native'
 
-const MealList = () => {
+const MealList = (props) => {
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.screen}>
+            <FlatList
+                data={displayedMeals}
+                keyExtractor={(item, index) => item.id}
+                renderItem={renderMealItem}
+                style={{ width: '100%' }}
+            />
         </View>
     )
 }
 
 export default MealList
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    
+})
