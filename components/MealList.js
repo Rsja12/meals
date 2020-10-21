7886b607d6be977fa24d920ec572b92c270ace84
Rsja12 <asocarras10@gmail.com>
2020-10-21 09:48:17 -0400

    rename StyleSheet property in MealList

--- components/MealList.js
+++ components/MealList.js
@@ -3,7 +3,7 @@ import { StyleSheet, View, FlatList } from 'react-native'
 
 const MealList = (props) => {
     return (
-        <View style={styles.screen}>
+        <View style={styles.list}>
             <FlatList
                 data={displayedMeals}
                 keyExtractor={(item, index) => item.id}
@@ -17,7 +17,7 @@ const MealList = (props) => {
 export default MealList
 
 const styles = StyleSheet.create({
-    screen: {
+    list: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
