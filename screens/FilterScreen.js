0b3f1fd0b72308b4682202dd752d44f540ea0581
Rsja12 <asocarras10@gmail.com>
2020-10-27 10:00:40 -0400

    add menu icon to FilterScreen

--- screens/FilterScreen.js
+++ screens/FilterScreen.js
@@ -1,24 +1,40 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import CustomHeaderButton from '../components/CustomHeaderButton';
 
 const FilterScreen = (props) => {
     return (
         <View style={styles.screen}>
             <Text>Filter Screen</Text>
         </View>
-    )
-}
+    );
+};
 
-FilterScreen.navigationOptions = {
-    headerTitle: 'Filter Meals'
-}
+FilterScreen.navigationOptions = (navData) => {
+    return {
+        headerTitle: 'Filter Meals',
+        headerLeft: (
+            <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+                <Item
+                    title='Menu'
+                    iconName='ios-menu'
+                    onPress={() => {
+                        navData.navigation.toggleDrawer();
+                    }}
+                />
+            </HeaderButtons>
+        ),
+    };
+};
 
-export default FilterScreen
+export default FilterScreen;
 
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
-})
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
